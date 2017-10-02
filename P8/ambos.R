6d49022f4b40d3ae3322f9214dc911c5aa5d1b7f
ValeryGomez <30806926+ValeryGomez@users.noreply.github.com>
2017-10-02 17:19:30 -0500

    Add files via upload

--- P8/ambos.R
+++ P8/ambos.R
@@ -1,7 +1,27 @@
+library('ggplot2')
 pa <- as.data.frame(read.csv("tiempos.csv"))
 sec <-as.data.frame(read.csv("Stiempos.csv"))
 ambos<- data.frame()
 ambos<-rbind(pa,sec)
-png(paste("ambos.png", sep=""), width=300, height=300)
-boxplot(pa$Tiempos~pa$k, color="red")
+ambos$k <- as.factor(ambos$k)
+png(paste("ambos.png", sep=""), width=700, height=700)
+ggplot(data=ambos,aes(x=k,y=Tiempos,fill=Tipo))+geom_boxplot()#stat_summary(fun.y=mean,geom="smooth",aes(group=Tipo,col=Tipo))
+#boxplot(pa$Tiempos~pa$k, color="red")
 graphics.off()
+
+####
+png(paste("Vnoparalelo.png", sep=""), width=700, height=700)
+boxplot(sec$Tiempos~sec$k)
+graphics.off()
+####
+png(paste("Vparalelo.png", sep=""), width=700, height=700)
+boxplot(pa$Tiempos~pa$k)
+graphics.off()
+###
+
+
+
+################Prueba Estadistica##################
+wilcox.test(ambos$Tiempos[ambos$Tipo=="Paralelo"],ambos$Tiempos[ambos$Tipo=="NoParalelo"] )
+median(ambos$Tiempos[ambos$Tipo=="Paralelo"])
+median(ambos$Tiempos[ambos$Tipo=="NoParalelo"])

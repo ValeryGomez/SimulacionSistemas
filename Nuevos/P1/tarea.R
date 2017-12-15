1954e6f1d395f45f0373816ef5a72f6578839226
ValeryGomez <30806926+ValeryGomez@users.noreply.github.com>
2017-12-15 16:53:09 -0600

    Add files via upload

--- Nuevos/P1/tarea.R
+++ Nuevos/P1/tarea.R
@@ -1,17 +1,18 @@
 repetir <- 100
-dd <- c(200,500,700)
+dd <- c(50,80,150,200)
 eucl <- TRUE
 dimm <- 8
 library(parallel)
-
+library('ggplot2')
 cluster <- makeCluster(detectCores() - 1)
-clusterExport(cluster, "duracion")
-clusterExport(cluster, "eucl")
+
 datos <- data.frame()
 graficas<- data.frame()
 porcientos <- c()
 for (l in 1:length(dd)){
   duracion <- dd[l]
+  clusterExport(cluster, "duracion")
+  clusterExport(cluster, "eucl")
 for (dimension in 1:dimm) {
   clusterExport(cluster, "dimension")
   resultado <- parSapply(cluster, 1:repetir,
@@ -38,38 +39,46 @@ for (dimension in 1:dimm) {
                              if(all(pos==0))
                                centro <- centro + 1
                            }
-                           todo <- c(mayor,centro)
+                           todo <- c(duracion,dimension,mayor,centro)
                            return(todo)
                          })
   datos <- rbind(datos, resultado)
 }
+  
 }
 stopCluster(cluster)
 
 datos<- t(datos)
+graff <- data.frame()
 write.csv(datos,file = "datos.csv")
-
-for(i in 1:(2*dimm)){
-  if(i%%2==0){
-    porcentaje<- sum(datos[,i])/repetir
+la <- 1
+valores <- c()
+colnames(datos)= rep(seq(1:4),dimm*length(dd))
+for(i in 1:(dimm*length(dd)-1)){
+  graff <- datos[,la:(la+3)]
+  graficas <- rbind(graficas,graff)
+  la <- la + 4
+    porcentaje<- sum(datos[,la-1])/repetir
     porcientos <- c(porcientos, porcentaje)
-  }else{
-    graficas <- rbind(graficas, datos[,i])
-  } 
 }
+colnames(graficas)= c("Pasos","Dimension", "Valor M", "Origen")
+#graficas <- t(graficas)
+graficas$Dimension <- as.factor(graficas$Dimension)
 
+#png(paste("Porcentajes.png", sep=""), width=700, height=700)
+ggplot(data=graficas,aes(x=Dimension,y=Origen,fill=Pasos))+geom_boxplot()+ylab("Cantidad de llegadas al Origen")
 
-if (eucl) {
-  png("p1er.png")
-  boxplot(data.matrix(graficas), use.cols=FALSE, 
-          xlab="Dimensi\u{F3}n", ylab="Distancia m\u{E1}xima", 
-          main="Euclideana")
-} else {
-  png("p1mr.png")
-  boxplot(data.matrix(graficas), use.cols=FALSE, 
-          xlab="Dimensi\u{F3}n", ylab="Distancia m\u{E1}xima", 
-          main="Manhattan")
-}
-graphics.off()
+#if (eucl) {
+#  png("p1er.png")
+#  boxplot(data.matrix(graficas), use.cols=FALSE, 
+#          xlab="Dimensi\u{F3}n", ylab="Distancia m\u{E1}xima", 
+#          main="Euclideana")
+#} else {
+#  png("p1mr.png")
+#  boxplot(data.matrix(graficas), use.cols=FALSE, 
+#          xlab="Dimensi\u{F3}n", ylab="Distancia m\u{E1}xima", 
+#          main="Manhattan")
+#}
+#graphics.off()
 
 
